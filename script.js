906013531224b89799363e0aad42acd1917c677f
Kristian <kr.mendoza.sh@gmail.com>
2023-03-03 13:13:10 -0500

    added edit book pop up when book is clicked

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 let myLibrary = [];
 const library= document.getElementById("library")
 
-function book(title, author, pages, readStatus){
+function book(title, author, pages, readStatus, bookNumber){
     this.title = title
     this.author = author
     this.pages = pages
     this.readStatus =readStatus
-    this.info = function() {
-        return (title + " by " + author + ", " + pages + " pages" +
-        ", " + readStatus);
-    }
+    this.bookNumber= bookNumber
 }
 
 function addBookToLibrary(title, author, pages, readStatus) {
-  myLibrary.push(new book(title, author, pages, readStatus));
-  addBookToGrid(myLibrary[myLibrary.length-1])
-  return;
+    let bookNumber="book"+(myLibrary.length+1).toString();
+    myLibrary.push(new book(title, author, pages, readStatus, bookNumber));
+    addBookToGrid(myLibrary[myLibrary.length-1])
+    return;
 }
 
 function addBookToGrid(book){
@@ -25,6 +23,8 @@ function addBookToGrid(book){
     let pages = document.createElement("p")
     let readStatus = document.createElement("p")
     newBook.classList.add("book")
+    newBook.dataset.bookNumber=book.bookNumber
+
     title.innerHTML=book.title
     author.innerHTML="by " + book.author
     pages.innerHTML="Pages: " + book.pages
@@ -37,17 +37,11 @@ function addBookToGrid(book){
     newBook.appendChild(author)
     newBook.appendChild(readStatus)
     library.appendChild(newBook)
+    newBook.addEventListener('click', e => {
+        openEditForm(e.target.closest('.book').dataset.bookNumber)
+    })
     return;
 }
-
-function updateBooks(){
-    for (let i=0; i<myLibrary.length; i++){
-        console.log(myLibrary[i].info);
-    }
-}
-
-
-console.log(myLibrary)
 
 /* Form buttons */
 
@@ -58,6 +52,10 @@ const overlay=document.getElementById("overlay");
 const openMenuButton = document.getElementById("menuButton");
 const closeMenuButton =document.getElementById("closeButton");
 
+const openEditButtons= document.querySelectorAll("[data-book-number]");
+const submitEditButton= document.getElementById("submitEditButton");
+const closeEditButton= document.getElementById("closeEditButton");
+
 function openMenu(){
     document.getElementById("menu").classList.add("active")
 }
@@ -67,15 +65,36 @@ function closeMenu(){
 }
 
 function openForm(){
-    document.getElementById("formContainer").classList.add('active')
+    document.getElementById("addBookFormContainer").classList.add('active')
     document.getElementById("overlay").classList.add('active')
     return;
 }
 
 function closeForm(){
-    document.getElementById("formContainer").classList.remove('active')
+    document.getElementById("addBookFormContainer").classList.remove('active')
     document.getElementById("overlay").classList.remove('active')
     return;
+}
+
+function openEditForm(bookNumber){
+    let title= document.getElementById("titleDisplay")
+    let author= document.getElementById("authorDisplay")
+    let pages= document.getElementById("pagesDisplay")
+    let currentBook=myLibrary[myLibrary.findIndex(book=>book.bookNumber==bookNumber)]
+    title.value=currentBook.title
+    author.value=currentBook.author
+    pages.value=currentBook.pages
+    document.getElementById("editBookFormContainer").classList.add('active')
+    document.getElementById("overlay").classList.add('active')
+}
+
+function closeEditForm(){
+    document.getElementById("editBookFormContainer").classList.remove('active')
+    document.getElementById("overlay").classList.remove('active')
+}
+
+function submitEditForm(){
+
 }
 
 openFormButton.addEventListener('click', openForm)
@@ -84,13 +103,22 @@ openMenuButton.addEventListener('click', openMenu)
 closeMenuButton.addEventListener('click', closeMenu)
 
 
+submitEditButton.addEventListener('click', submitEditForm)
+closeEditButton.addEventListener('click', closeEditForm)
+
+
+
 submitFormButton.addEventListener('click', ()=>{
     let title=document.getElementById("title").value
     let author=document.getElementById("author").value
     let pages=document.getElementById("pages").value
-    let readStatus=document.getElementById("readStatus").value
+    let readStatus=document.querySelector("input[name='readStatus']:checked").value
     addBookToLibrary(title, author, pages, readStatus)
     closeForm();
     return;
 })
 
+/*when book is clicked, open a form to update the book's properties,
+add event listener update button in form to call function that retrieves
+what library index the book is in, then changes its properties with the changes 
+the user made*/
